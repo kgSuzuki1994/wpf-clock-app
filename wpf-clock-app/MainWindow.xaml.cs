114105b5f8546e441d079be1bfa418219a3fa992
kgSuzuki1994 <takuti0511@gmail.com>
2020-09-05 16:43:31 +0900

    タイマー生成メソッドの追加

--- wpf-clock-app/MainWindow.xaml.cs
+++ wpf-clock-app/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace wpf_clock_app
 {
@@ -20,9 +21,39 @@ namespace wpf_clock_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 時計表示用タイマー
+        /// </summary>
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// タイマー生成処理
+        /// </summary>
+        /// <returns>生成したタイマー</returns>
+        private DispatcherTimer CreateTimer()
+        {
+            // タイマー生成(優先度はアイドル時に設定)
+            var t = new DispatcherTimer(DispatcherPriority.SystemIdle);
+
+            // タイマーイベントの発生間隔を300ミリ秒に設定
+            t.Interval = TimeSpan.FromMilliseconds(300);
+
+            // タイマーイベントの定義
+            t.Tick += (sender, e) =>
+            {
+                // タイマーイベント発生時の処理をここに書く
+
+                // 現在の時分秒をテキストに設定
+                textBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            // 生成したタイマーを返す
+            return t;
         }
     }
 }
